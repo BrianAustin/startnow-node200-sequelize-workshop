3e5a4ed1bf55449b58ce943868cc7db7c6a4a235
Brian <baustin27@gmail.com>
2018-06-05 15:08:06 -0700

    blog paths

--- server/routes/Blogs.js
+++ server/routes/Blogs.js
@@ -2,9 +2,56 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/models/index');
 
+router.get('/', (req, res) => {
+  db.Blog
+  .findAll()
+  .then(blogs => {
+    res.status(200).json(blogs)
+  }).catch(err => res.status(404).send('bad get'));
+});
 
+router.get('/featured', (req, res) => {
+  db.Blog
+    // .where('featured', true)
+    .findAll({ where: { featured: true }})
+    .then(blogs => {
+      res.status(200).json(blogs)
+    });
+});
 
+router.get('/:id', (req, res) => {
+  db.Blog
+    .findById(req.params.id)
+    .then(blog => {
+      if (!blog) res.status(404).send('blog not found');
+      res.status(200).json(blog)
+    }).catch(err => res.status(404).send('bad id get'));
+});
 
+router.post('/', (req, res) => {
+  let newBlog = req.body;
+  newBlog.authorId = req.query.authorId;
+  db.Blog
+    .create(newBlog)
+    .then(blogs => {
+      res.status(201).json(blogs)
+    });
+});
 
+router.put('/:id', (req, res) => {
+  db.Blog
+    .update(req.body, {where: { id: req.params.id }})
+    .then(author => {
+      res.status(204).json(author)
+    }).catch(err => res.status(500).send('bad put'));
+});
+
+router.delete('/:id', (req, res) => {
+  db.Blog
+  .destroy({where: { id: req.params.id }})
+  .then(blog => {
+    res.status(200).json(blog)
+  }).catch(err => res.status(404).send('bad delete'));
+});
 
 module.exports = router;
